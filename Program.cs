bf0898f041a9861816e9329195cf0f26d7f34440
pageza <zap.freelance@gmail.com>
2017-07-02 14:07:31 -0500

    testing more

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace first_app
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hellofsdfsd  World!");
         }
     }
 }
